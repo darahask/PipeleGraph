250aa237ffb5fad2fc82625a723407aa617664b4
darahask <darahask12@gmail.com>
2022-07-19 15:25:07 +0530

    add graph query scripts

---
+++ scripts/queries.js
@@ -0,0 +1,42 @@
+const axios = require("axios").default;
+
+let URL = "https://api.thegraph.com/subgraphs/name/darahask/pipe";
+
+let getIdofFileQuery = (cid) => `{
+    pipeleEntities(
+      where: {cid: "${cid}"}
+    ) {
+      id,
+      owner
+    }
+  }`;
+
+let getSharedofOwnerQuery = (owner) => `{
+    pipeleEntities(where: {owner: "${owner}"}) {
+      shared,
+      id
+      cid,
+    }
+  }`;
+
+let getData = async (func, val) => {
+  let query = func(val);
+  let res = await axios.post(URL, { query });
+  console.log(res.data.data);
+};
+
+getData(
+  getSharedofOwnerQuery,
+  "0xdd372842cb80c1892243d20ee4ad0979c293cad5"
+).catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+getData(
+  getIdofFileQuery,
+  "bafybeibhirdk3junquvmanfrfknedqqx5uwkdbmuo24fssxpuviicjpm4u"
+).catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
