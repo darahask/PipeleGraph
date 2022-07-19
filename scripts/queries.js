e9dd0afd5a7e184b42143fcee97ee10a2b97f2ac
darahask <darahask12@gmail.com>
2022-07-19 17:00:40 +0530

    add new query script

--- scripts/queries.js
+++ scripts/queries.js
@@ -19,23 +19,41 @@ let getSharedofOwnerQuery = (owner) => `{
     }
   }`;
 
+let getAccessibleFiles = (
+  addr
+) => `{pipeleEntities(where: {shared_contains: ["${addr}"]}) {
+  id,
+  cid,
+  owner
+}}`;
+
 let getData = async (func, val) => {
   let query = func(val);
   let res = await axios.post(URL, { query });
   console.log(res.data.data);
 };
 
-getData(
-  getSharedofOwnerQuery,
-  "0xdd372842cb80c1892243d20ee4ad0979c293cad5"
-).catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+/* uncomment to test */
+
+// getData(
+//   getSharedofOwnerQuery,
+//   "0xdd372842cb80c1892243d20ee4ad0979c293cad5"
+// ).catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+
+// getData(
+//   getIdofFileQuery,
+//   "bafybeibhirdk3junquvmanfrfknedqqx5uwkdbmuo24fssxpuviicjpm4u"
+// ).catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
 
 getData(
-  getIdofFileQuery,
-  "bafybeibhirdk3junquvmanfrfknedqqx5uwkdbmuo24fssxpuviicjpm4u"
+  getAccessibleFiles,
+  "0xdd372842cb80c1892243d20ee4ad0979c293cad5"
 ).catch((error) => {
   console.error(error);
   process.exitCode = 1;
